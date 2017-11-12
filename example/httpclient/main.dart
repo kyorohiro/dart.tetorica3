176ec97c7032f63b9a983d1227adc42cc62c778e
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-11-13 03:21:03 +0900

    ..

---
+++ example/httpclient/main.dart
@@ -0,0 +1,9 @@
+import 'package:tetorica/data.dart' as tet;
+import 'package:tetorica/parser.dart' as tet;
+import 'package:tetorica/net.dart' as tet;
+import 'package:tetorica/net_dartio.dart' as tet;
+
+main() {
+  print("Hello World!!");
+  tet.TetSocketBuilder builder = new tet.TetSocketBuilderDartIO();
+}
