7164e8d2b1edb26e9fdb9390392a1a9db08a9246
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-11-13 03:42:37 +0900

    ..

--- example/httpclient/main.dart
+++ example/httpclient/main.dart
@@ -2,8 +2,21 @@ import 'package:tetorica/data.dart' as tet;
 import 'package:tetorica/parser.dart' as tet;
 import 'package:tetorica/net.dart' as tet;
 import 'package:tetorica/net_dartio.dart' as tet;
+import 'package:tetorica/http.dart' as tet;
 
-main() {
+main() async {
   print("Hello World!!");
-  tet.TetSocketBuilder builder = new tet.TetSocketBuilderDartIO();
+  tet.TetSocketBuilder socketBuilder = new tet.TetSocketBuilderDartIO();
+  tet.HttpClientPlus client = new tet.HttpClientPlus(socketBuilder);
+  tet.HttpClientResponse response = await client.get("www.google.co.jp", 80, "/");
+//  tet.HttpClientResponse response = await client.get("www.google.com", 80, "/");
+
+  print("# LEN : ${response.info.contentLength}");
+  for(tet.HttpResponseHeaderField f in response.info.headerField ) {
+    print("# HEA : " + f.fieldName + " " + f.fieldValue + "");
+  }
+  tet.TetReader reader = response.body;
+  String ret = await reader.getAllString();
+  print("${ret}");
+
 }
