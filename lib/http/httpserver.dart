135c9ad2e5f9b5f0309ce6e27c2dbc1ec3e48805
kyorohiro <kyorohiro@gmail.com>
2017-11-22 03:08:28 +0900

    ..

--- lib/http/httpserver.dart
+++ lib/http/httpserver.dart
@@ -4,29 +4,33 @@ part of hetimanet_http;
 class HttpServer {
 
   StreamController _controllerOnNewRequest = new StreamController.broadcast();
-//  HetimaSocketBuilder _builder;
-  String host;
-  int port;
+  String _bindIP;
+  int _port;
+  String get bindIP => _bindIP;
+  int get port => _port;
   ServerSocket _serverSocket = null;
   HttpServer._internal(ServerSocket s) {
     _serverSocket = s;
   }
 
   void close() {
-    if(_serverSocket != null) {
-      _serverSocket.close();
-      _serverSocket = null;
-      _controllerOnNewRequest.close();
-      _controllerOnNewRequest = null;
+    if(_serverSocket == null) {
+      return;
     }
+    _serverSocket.close();
+    _serverSocket = null;
+    _controllerOnNewRequest.close();
+    _controllerOnNewRequest = null;
   }
 
-  static Future<HttpServer> bind(SocketBuilder builder, String address, int port) async {
-    ServerSocket serverSocket = await builder.startServer(address, port);
+  static Future<HttpServer> bind(SocketBuilder builder, String bindIP, int port) async {
+    ServerSocket serverSocket = await builder.startServer(bindIP, port);
     if (serverSocket == null) {
-      throw "failed binding ${address}:${port}";
+      throw "failed binding ${bindIP}:${port}";
     }
     HttpServer server = new HttpServer._internal(serverSocket);
+    server._bindIP = bindIP;
+    server._port = port;
     serverSocket.onAccept().listen((Socket socket) async {
       EasyParser parser = new EasyParser(socket.buffer);
       HetiHttpRequestMessageWithoutBody body = await HetiHttpResponse.decodeRequestMessage(parser);
